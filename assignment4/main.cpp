d40711cca85ca9c29321020986e9d813a6ce4663
Mikel Matticoli <mamatticoli@wpi.edu>
2018-02-15 02:13:03 -0500

    assignment 4 handle/parse command line args

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include "Event.h"
 
-int main() {
-    double simDuration = 100;
+int main(int argc, char *argv[]) {
+    // If missing args, print usage
+    if(argc < 5) {
+        std::cout << "Usage:" << std::endl
+                  << "./qSim #customers #tellers simulationTime averageServiceTime <seed>" << std::endl;
+        return 1;
+    }
 
-    std::cout << "Hello, World!" << std::endl;
+    // Parse command line args
+    int customerCount = atoi(argv[1]);
+    int tellerCount = atoi(argv[2]);
+    double simDuration = atof(argv[3]);
+    double avgSvcTime = atof(argv[4]);
+
+    // If any args are invalid, print usage and >0 warning
+    if(!customerCount || !tellerCount || !simDuration || !avgSvcTime) {
+        std::cout << "Usage:" << std::endl
+                  << "./qSim #customers #tellers simulationTime averageServiceTime <seed>" << std::endl
+                  << "Note that all required parameters must be greater than 0" << std::endl;
+        return 1;
+    }
+
+    double simTime = 0;
+
     Event *e = new Event(2.0, 2.0);
     std::cout << e->toString() << std::endl;
 
-    double simTime = 0;
     while(simTime < simDuration) {
         simTime++;
     }
